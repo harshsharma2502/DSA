01cf292087593a22bc17cdee19e84aa8cbafb871
harshsharma2502 <connect2harshsharma@gmail.com>
2025-02-15 02:43:49 +0530

    Create 704 - Binary Search.java

---
+++ 704 - Binary Search.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/binary-search/description/
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length-1;
+        System.out.print(h);
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                h = mid-1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
