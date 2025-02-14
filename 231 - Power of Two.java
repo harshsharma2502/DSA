e86684f203c3e5c434e8780ef7da863441cf775b
harshsharma2502 <connect2harshsharma@gmail.com>
2025-02-15 02:35:51 +0530

    Create 231 - Power of Two.java

---
+++ 231 - Power of Two.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/power-of-two/description/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+
+        return (n%2 == 0) && isPowerOfTwo(n/2);
+    }
+}
