553f9156c206839072cfe30ebc4b50795c873eb1
harshsharma2502 <connect2harshsharma@gmail.com>
2025-06-11 02:29:18 +0530

    Time: 4 ms (56.28%) | Memory: 44.8 MB (78.28%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,60 +1,19 @@
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         for(int i=0; i<nums.length; i++){
-//             map.put(nums[i],i);
-//         }
-
-//         for(int i=0; i<nums.length; i++){
-//             int num = nums[i];
-//             int rem = target-num;
-//             if(map.containsKey(rem)){
-//                 int index = map.get(rem);
-//                 if(index == i)continue;
-//                 return new int[]{i,index};
-//             }
-//         }
-//         return new int[]{};
-//     }
-// }
-
-
-
-
-
-// Brute Force
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         int n = nums.length;
-//         for(int i=0; i<n; i++){
-//             for(int j=1; j<n; j++){
-//                 if(nums[i] + nums[j] == target){
-//                     return new int[]{i,j};
-//                 }
-//             }
-//         }
-//         return new int[]{};
-// }
-// }
-
-
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hm = new HashMap();
+        HashMap<Integer,Integer> map= new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            hm.put(nums[i],i);
+            map.put(nums[i],i);
         }
-        
         for(int i=0; i<nums.length; i++){
-            int rem = target - nums[i];
-            int index;
-            if(hm.containsKey(rem)){
-                index = hm.get(rem);
-                if(index == i)continue;
-                return new int[]{i,index};
+            int num = nums[i];
+            int rem = target - num;
+            if(map.containsKey(rem)){
+                int idx = map.get(rem);
+                if(idx == i) continue;
+            return new int[]{i,idx};
+        }
             }
-        }
+            
         return new int[]{};
     }
 }
