0f6ce874bb8e7984b1fe7cfe5f834c6b52e4dc58
harshsharma2502 <connect2harshsharma@gmail.com>
2025-06-14 03:30:33 +0530

    Time: 7 ms (36.26%) | Memory: 44.8 MB (25.27%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        if(s.length() != t.length()) return false;
+
+        char ss[] = s.toCharArray();
+        char tt[] = t.toCharArray();
+        Arrays.sort(ss);
+        Arrays.sort(tt);
+
+        return Arrays.equals(ss,tt);
+
+    }
+}
