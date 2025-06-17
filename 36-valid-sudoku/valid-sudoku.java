bd6d5abbd49b641ac8e23b12406c7991390f5e38
harshsharma2502 <connect2harshsharma@gmail.com>
2025-06-17 17:29:57 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (50.46%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] row = new int[9][9];
+        int[][] col = new int[9][9];
+        int[][] boxes = new int[9][9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+
+                int val = board[r][c] - '1'; // value 0 to 8
+
+                if (row[r][val] == 1) return false;
+                row[r][val] = 1;
+
+                if (col[c][val] == 1) return false;
+                col[c][val] = 1;
+
+                int boxIdx = (r / 3) * 3 + (c / 3);
+                if (boxes[boxIdx][val] == 1) return false;
+                boxes[boxIdx][val] = 1;
+            }
+        }
+
+        return true;
+    }
+}
