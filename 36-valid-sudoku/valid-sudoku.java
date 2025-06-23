5f04ca87df399e8cdf613bc1ec0b99c5e553e3a7
harshsharma2502 <connect2harshsharma@gmail.com>
2025-06-23 06:58:38 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (50.33%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int rows[][] = new int[9][9];
-        int cols[][] = new int [9][9];
+        int cols[][] = new int[9][9];
         int box[][] = new int[9][9];
 
-        for(int r=0; r<9; r++){
-            for(int c=0; c<9; c++){
-                if(board[r][c] == '.') continue;
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.') continue;
 
-                int val = board[r][c] - '1';
-                if(rows[r][val] == 1) return false;
-                rows[r][val] = 1;
+                int val = board[i][j] - '1';
+                if(rows[i][val] == 1) return false;
+                rows[i][val] = 1;
 
-                if(cols[val][c] == 1) return false;
-                cols[val][c] = 1;
+                if(cols[j][val] == 1)return false;
+                cols[j][val] = 1;
 
-                int boxIdx = 3 * (r/3) + (c/3);
+                int boxIdx = (i / 3) * 3 + (j / 3);
                 if(box[boxIdx][val] == 1) return false;
-                box[boxIdx][val] = 1;
+                box[boxIdx][val] =1;
             }
         }
         return true;
